4fb79ee1dc62ec269e8a735f6c7bc05ada6dadd2
Chandrima Kar <chandrimakar16@gmail.com>
2024-06-06 12:56:07 +0530

    Added movie review form, and movie reviews

---
+++ client/src/components/Reviews/index.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import user_img from '../../pages/SingleMovie/chandrima.jpg';
+import { FaStar } from "react-icons/fa6";
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import '../../pages/SingleMovie/styles.css';
+
+const Reviews = ({ reviews }) => {
+  const sliderSettings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1032,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    reviews.length > 0 ? (
+      <div className="text-left text-white flex flex-col gap-1 common-container">
+        <h2 className="common-heading">Reviews:</h2>
+        <Slider {...sliderSettings}>
+          {reviews.map((review, index) => (
+            <div key={index} className="flex flex-row gap-5 mb-5 mt-5 p-5">
+              <div className="relative p-5 bg-[#010b14] h-60 border border-gray-300 rounded-lg shadow-md flex flex-col">
+                <div className="sticky-top flex items-center gap-2">
+                  <div className="flex-shrink-0 w-10 h-10">
+                    <img src={user_img} className="w-full h-full rounded-full object-cover" alt="User" />
+                  </div>
+                  <div className="text-white font-semibold">Anonymous</div>
+                </div>
+                <div className="sticky-top absolute top-7 right-5 text-red-600 font-bold text-md flex items-center gap-1">
+                  <FaStar />
+                  {review.rating}/5
+                </div>
+                <div className="mt-4 text-left flex-grow review-text-container items-center">
+                  <p className="text-white text-sm review-text">
+                    {review.comment}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    ) : (
+      <div></div>
+    )
+  );
+};
+
+export default Reviews;
