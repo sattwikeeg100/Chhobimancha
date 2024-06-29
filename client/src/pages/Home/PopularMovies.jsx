e074f1a3dadd69ab8e21a79f1a2a653c4a9e7be4
RoshanPathak1234 <roshanpathak174@gmail.com>
2024-06-29 20:37:04 +0530

    home page added

---
+++ client/src/pages/Home/PopularMovies.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+// import SwitchTabs from '../../SwitchTabs/SwitchTabs'
+import movies from "./movies.js"
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+const PopularMovies = () => {
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }
+        ]
+    };
+
+    return (
+        <div id='' className=' m-auto w-[90%] ' >
+            <h3 id='' className=' text-2xl text-white font-normal' >
+                Popular movies
+            </h3>
+            <div className='my-5' >
+                <Slider {...settings}>
+
+
+                    {
+                        movies.map((movie, key) => (
+                            <Link to="explore/shows/:slug" >
+                                <div id={key} className=' font-normal '>
+                                    <div className='p-2 flex justify-center items-center flex-wrap ' >
+                                        <div id='moviePoster' className={' w-full text-center bg-cover '} >
+                                            <img src={movie.image} alt="" className='h-full w-full bg-contain' />
+                                            <div className='my-3 text-xl '>
+                                                <h3>
+                                                    {movie.title}
+                                                </h3>
+                                            </div>
+                                        </div>
+                                        <div id='movieDescription' className='' >
+                                            {movie.Description + "  " + key}
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </Link>
+                        ))
+                    }
+                </Slider>
+            </div>
+        </div>
+    )
+}
+
+export default PopularMovies
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,10 +1,85 @@
 import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import PopularMovies from "./PopularMovies";
+import TrendingMovies from "./TrendingMovies";
+import LatestMovies from "./LatestMovies";
+import { recentMovies } from "./movies";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Home = () => {
+    const [oldSlide, setOldSlide] = useState(0);
+  const [activeSlide, setActiveSlide] = useState(0);
+  const [activeSlide2, setActiveSlide2] = useState(0);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay:true,
+    speed: 1000,
+     autoplaySpeed: 1000,
+    cssEase: "linear",
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    beforeChange: (current, next) => {
+      setOldSlide(current);
+      setActiveSlide(next);
+    },
+    afterChange: current => setActiveSlide2(current)
+  };
+    const [searchval, setSearchval] = useState("");
+    const searchbarHandler = (event) => {
+        const query = event.target.value;
+        if (event.key === "Enter" && query.length > 0) {
+            setSearchval(query);
+            navigate(`/search/${query}`);
+        }
+    };
     return (
-        <div className="text-5xl mt-32 justify-center items-center mb-[50vh] p-10">
-            <h1> Welcome Home !! </h1>
-        </div>
+        <div id="Home" className=" pb-16 w-screen h-fit flex flex-col items-center bg-black text-white font-semibold font-serif " >
+
+            < header className='h-full w-full my-8'>
+                <div className={` w-full h-full`}  >
+                    <div className="w-full my-4 h-fit flex justify-center items-center flex-nowrap ">
+                        <div className="">
+                            <input
+                                className=' text-white font-semibold font-serif  p-5 caret-white border-solid border-white border-2 border-r-0 rounded-l-3xl active:border-none w-20vw  lg:w-[50vw] md:w-[30vw] text-xl overflow-x-hidden bg-transparent '
+                                type="text"
+                                placeholder="Search for a movie or tv show...."
+                                onChange={(e) => setSearchval(e.target.value)}
+                                onKeyUp={searchbarHandler}
+                            />
+                            <input className="p-5 border-solid border-white border-2 border-l-0 hover:border-none rounded-r-3xl bg-yellow-500 text-white text-xl" type="button" value="Search" placeholder="Search" />
+                        </div>
+                    </div>
+                    <Slider {...settings} className="mx-10 object-center ">
+
+                        {
+                            recentMovies.map((movie, key) => (
+                                <Link to="explore/shows/:slug" >
+                                <div className="">
+                                    <img src={`${movie.image}`} alt="" className="w-[90%] h-[40vw] md:h-[30vw] object-center" />
+
+                                </div>
+                                </Link>
+                            ))
+                        }
+                    </Slider>
+                </div>
+            </header >
+            <div className="w-screen h-full flex justify-center items-center">
+                <div className=" w-full h-full" >
+
+                    <LatestMovies />
+                    <PopularMovies />
+                    <TrendingMovies />
+                </div>
+
+            </div>
+
+        </div >
     );
 };
 
