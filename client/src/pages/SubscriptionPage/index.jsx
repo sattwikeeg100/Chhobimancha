77d8db38301abc4af92468b13f477d13505a9b8e
Sattwikee Ghosh <sattwikeeghosh95@gmail.com>
2024-07-03 22:07:27 +0530

    user feedback added

--- client/src/pages/SubscriptionPage/index.jsx
+++ client/src/pages/SubscriptionPage/index.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import axiosInstance from "../../config/axiosInstance.js";
 import { updateUser } from "../../store/slices/userSlice.js";
+import { switchLoginModalOpen } from "../../store/slices/loginModalOpenSlice.js";
+import { toast } from "sonner";
 
 const SubscriptionPage = () => {
+    const [showModal, setShowModal] = useState(false);
+    const [formData, setFormData] = useState({});
+    const [submitting, setSubmitting] = useState(false);
+
     const APIURL = import.meta.env.VITE_API_URL;
     const dispatch = useDispatch();
+    const FEEDBACK_URL = import.meta.env.VITE_GOOGLE_WEB_APP_URL;
 
     const user = useSelector((state) => state.user.userInfo);
 
     const handlePayment = async () => {
+        if (!user) {
+            dispatch(switchLoginModalOpen(true));
+            return;
+        }
+
         const razorpayKey = import.meta.env.VITE_RAZORPAY_KEY_ID;
         const razorpayPlan = import.meta.env.VITE_RAZORPAY_PLAN_ID;
         try {
@@ -92,6 +104,51 @@ const SubscriptionPage = () => {
             alert("Error cancelling subscription. Please try again.");
         }
     };
+
+    const handleFeedbackChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleEmojiClick = (emoji) => {
+        setFormData({ ...formData, experience: emoji });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setSubmitting(true);
+        const currTimestamp = new Date();
+        let feedbackData;
+        if (user) {
+            feedbackData = {
+                ...formData,
+                name: user.name,
+                email: user.email,
+                time: currTimestamp,
+            };
+        } else {
+            feedbackData = { ...formData, time: currTimestamp };
+        }
+
+        console.log({ ...formData, time: currTimestamp });
+        try {
+            const response = await fetch(FEEDBACK_URL, {
+                method: "POST",
+                body: JSON.stringify(feedbackData),
+            });
+            const result = await response.json();
+            if (result.status === "success") {
+                toast.success("Feedback submitted successfully!");
+                setSubmitting(false);
+                setShowModal(false);
+                setFormData({});
+            }
+        } catch (error) {
+            console.error("Error submitting feedback:", error);
+            toast.error("Failed to submit feedback. Please try again.");
+        }
+    };
+    console.log(formData);
 
     return (
         <div className="min-h-screen bg-gray-100 py-12">
@@ -194,6 +251,118 @@ const SubscriptionPage = () => {
                         )}
                     </div>
                 </div>
+            </div>
+            <div className="App">
+                <button
+                    className="fixed right-0 bottom-1/2  rotate-90 bg-blue-500 text-white py-2 px-4 rounded-lg"
+                    onClick={() => setShowModal(true)}>
+                    Give feedback
+                </button>
+
+                {showModal && (
+                    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+                        <div className="bg-white rounded-lg p-6 w-full max-w-lg">
+                            <h2 className="text-2xl mb-4">Feedback</h2>
+                            <form onSubmit={handleSubmit}>
+                                {!user && (
+                                    <>
+                                        <div className="mb-4">
+                                            <label
+                                                className="block text-gray-700 text-sm font-bold mb-2"
+                                                htmlFor="email">
+                                                Name
+                                            </label>
+                                            <input
+                                                type="text"
+                                                name="name"
+                                                value={formData.name}
+                                                onChange={handleFeedbackChange}
+                                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                                required
+                                            />
+                                        </div>
+                                        <div className="mb-4">
+                                            <label
+                                                className="block text-gray-700 text-sm font-bold mb-2"
+                                                htmlFor="email">
+                                                Email
+                                            </label>
+                                            <input
+                                                type="email"
+                                                name="email"
+                                                value={formData.email}
+                                                onChange={handleFeedbackChange}
+                                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                                required
+                                            />
+                                        </div>
+                                    </>
+                                )}
+                                <div className="mb-4">
+                                    <label
+                                        className="block text-gray-700 text-sm font-bold mb-2"
+                                        htmlFor="experience">
+                                        Rate your experience
+                                    </label>
+                                    <div className="flex space-x-2">
+                                        {["😃", "😊", "😐", "☹️", "😠"].map(
+                                            (emoji) => (
+                                                <button
+                                                    type="button"
+                                                    key={emoji}
+                                                    className={`text-2xl ${
+                                                        formData.experience ===
+                                                        emoji
+                                                            ? "border border-blue-500"
+                                                            : ""
+                                                    }`}
+                                                    onClick={() =>
+                                                        handleEmojiClick(emoji)
+                                                    }>
+                                                    {emoji}
+                                                </button>
+                                            )
+                                        )}
+                                    </div>
+                                </div>
+                                <div className="mb-4">
+                                    <label
+                                        className="block text-gray-700 text-sm font-bold mb-2"
+                                        htmlFor="message">
+                                        Message
+                                    </label>
+                                    <textarea
+                                        name="feedback"
+                                        value={formData.feedback}
+                                        onChange={handleFeedbackChange}
+                                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                        required></textarea>
+                                </div>
+                                <div className="flex items-center justify-between">
+                                    <button
+                                        type="button"
+                                        className="bg-gray-500 text-white py-2 px-4 rounded-lg mr-2"
+                                        onClick={() => setShowModal(false)}>
+                                        Cancel
+                                    </button>
+                                    {submitting ? (
+                                        <button
+                                            type="submit"
+                                            className="bg-blue-400 text-white py-2 px-4 rounded-lg cursor-not-allowed">
+                                            Submitting...
+                                        </button>
+                                    ) : (
+                                        <button
+                                            type="submit"
+                                            className="bg-blue-500 text-white py-2 px-4 rounded-lg">
+                                            Submit
+                                        </button>
+                                    )}
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );
