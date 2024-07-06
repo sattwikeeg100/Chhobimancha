4e4328674e9406eb5077fcee797d279baff69312
Pratik Biswas <04biswaspratik@gmail.com>
2024-07-06 13:23:27 +0530

    new change

--- client/src/components/castSlider/index.jsx
+++ client/src/components/castSlider/index.jsx
@@ -1,5 +1,5 @@
-import React, { useRef, useState, useEffect } from 'react';
-import '../../pages/SingleMovie/styles.css';
+import React, { useRef, useState, useEffect } from "react";
+import "../../pages/SingleMovie/styles.css";
 import {
   IoIosArrowDropleftCircle,
   IoIosArrowDroprightCircle,
@@ -56,12 +56,21 @@ const CastSlider = ({ casts, who }) => {
           <div className="w-[2%] p-2 mr-2" /> // Placeholder for left side gap
         )}
 
-        <div ref={sliderRef} className="flex overflow-x-auto gap-[3rem] hide-scrollbar">
+        <div
+          ref={sliderRef}
+          className="flex overflow-x-auto gap-[3rem] hide-scrollbar"
+        >
           {casts.map((cast, index) => (
             <div key={index} className="flex-shrink-0 w-32">
-              <img src={cast.person.image} alt={cast.person.name} className="w-28 h-28 rounded-full mb-2" />
+              <img
+                src={cast.person.image}
+                alt={cast.person.name}
+                className="w-28 h-28 rounded-full mb-2 object-cover"
+              />
               <div className="text-center flex flex-col">
-                <div className="font-semibold font-lato text-white">{cast.person.name}</div>
+                <div className="font-semibold font-lato text-white">
+                  {cast.person.name}
+                </div>
                 <div className="text-white font-roboto">{cast.role}</div>
               </div>
             </div>
