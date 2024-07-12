7f879deb2e6ab7b76e5cd85085c8b9b4def528f6
RoshanPathak1234 <roshanpathak174@gmail.com>
2024-07-13 03:31:03 +0530

    home page problem fixes and admin dashboard updated

--- client/src/pages/AdminDashboard/index.jsx
+++ client/src/pages/AdminDashboard/index.jsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Bar, Pie } from "react-chartjs-2";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./CustomSlick.css";
 import {
     Chart as ChartJS,
     CategoryScale,
@@ -134,13 +138,53 @@ const AdminDashboard = () => {
             },
         ],
     };
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 650,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    };
+
 
     return (
-        <>
-            <div className="container mx-auto">
-                <h1 className="text-2xl font-bold mb-4">Admin Dashboard</h1>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4">
-                    <div className="bg-white p-4 rounded-lg shadow-md">
+        <div className=" mr-3">
+            <div className="container max-w-screen md:max-w-[900px] mx-auto overflow-x-hidden h-40">
+                <h1 className="text-2xl font-bold mb-4 text-white ">Admin Dashboard</h1>
+                <div className=" w-full">
+                <Slider {...settings} id="AdminDashboardSlider" className="mx-14 ">
+                    <div className="bg-white p-4 rounded-lg shadow-md !w-60 lg:!mr-7 md:!mr-11 ">
                         <h2 className="text-lg font-semibold mb-2">Users</h2>
                         {loading ? (
                             <p>Loading...</p>
@@ -148,7 +192,7 @@ const AdminDashboard = () => {
                             <p>Total Users: {allInfos.totalUsers}</p>
                         )}
                     </div>
-                    <div className="bg-white p-4 rounded-lg shadow-md">
+                    <div className="bg-white p-4 rounded-lg shadow-md !w-60">
                         <h2 className="text-lg font-semibold mb-2">
                             Subscribers
                         </h2>
@@ -160,7 +204,7 @@ const AdminDashboard = () => {
                             </p>
                         )}
                     </div>
-                    <div className="bg-white p-4 rounded-lg shadow-md">
+                    <div className="bg-white p-4 rounded-lg shadow-md !w-60">
                         <h2 className="text-lg font-semibold mb-2">Movies</h2>
                         {loading ? (
                             <p>Loading...</p>
@@ -168,7 +212,7 @@ const AdminDashboard = () => {
                             <p>Total Movies: {allInfos.totalMovies}</p>
                         )}
                     </div>
-                    <div className="bg-white p-4 rounded-lg shadow-md">
+                    <div className="bg-white p-4 rounded-lg shadow-md !w-60">
                         <h2 className="text-lg font-semibold mb-2">Shows</h2>
                         {loading ? (
                             <p>Loading...</p>
@@ -176,7 +220,7 @@ const AdminDashboard = () => {
                             <p>Total Shows: {allInfos.totalShows}</p>
                         )}
                     </div>
-                    <div className="bg-white p-4 rounded-lg shadow-md">
+                    <div className="bg-white p-4 rounded-lg shadow-md !w-60">
                         <h2 className="text-lg font-semibold mb-2">
                             Show Bookings
                         </h2>
@@ -186,31 +230,32 @@ const AdminDashboard = () => {
                             <p>Total Bookings: {allInfos.totalBookings}</p>
                         )}
                     </div>
+                    </Slider>
                 </div>
             </div>
-            <div className="flex flex-col w-screen gap-10 mt-10 justify-center items-center">
-                <div className="flex flex-row gap-10">
-                    <div>
-                        <h2>Total Revenue</h2>
-                        <Pie data={pieData} />
+            <div className=" w-[90%]  flex flex-col my-20 justify-around items-stretch flex-wrap">
+                <div className="flex flex-row  flex-wrap justify-around items-stretch">
+                    <div className="bg-background2 p-5 rounded-lg my-10">
+                        <h2 className="text-white font-montserrat my-5">Total Revenue</h2>
+                        <Pie data={pieData} className="bg-gray-950 " />
                     </div>
-                    <div>
-                        <h2>Overall Statistics</h2>
-                        <Bar data={consolidatedData} />
+                    <div className="bg-background2 p-5 rounded-lg my-10">
+                        <h2 className="text-white font-montserrat my-5">Overall Statistics</h2>
+                        <Bar data={consolidatedData} className="bg-gray-950 "  />
                     </div>
                 </div>
-                <div className="flex flex-row gap-10">
-                    <div>
-                        <h2>Booking Revenue (Last 6 Months)</h2>
-                        <Bar data={bookingData} />
+                <div className="flex flex-row gap-10 my-10 flex-wrap justify-around items-stretch">
+                    <div className="bg-background2 p-5 rounded-lg my-10">
+                        <h2 className="text-white font-montserrat my-5">Booking Revenue (Last 6 Months)</h2>
+                        <Bar data={bookingData} className="bg-gray-950 "  />
                     </div>
-                    <div>
-                        <h2>Subscription Revenue (Last 6 Months)</h2>
-                        <Bar data={subscriptionData} />
+                    <div className="bg-background2 p-5 rounded-lg my-10" >
+                        <h2 className="text-white font-montserrat my-5">Subscription Revenue (Last 6 Months)</h2>
+                        <Bar data={subscriptionData} className="bg-gray-950 "  />
                     </div>
                 </div>
             </div>
-        </>
+            </div>
     );
 };
 
