7f4ef8950dbe4a0e3f3c18e4bd97b2ae79e3adec
Pratik Biswas <04biswaspratik@gmail.com>
2024-07-13 04:54:40 +0530

    my bookings page is updated

--- client/src/pages/MyBookings/index.jsx
+++ client/src/pages/MyBookings/index.jsx
@@ -12,7 +12,11 @@ const MyBookings = () => {
     const fetchBookings = async () => {
       try {
         const response = await axiosInstance.get(`/bookings`);
-        setBookings(response.data);
+        // Sort bookings by createdAt date in descending order
+        const sortedBookings = response.data.sort(
+          (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+        );
+        setBookings(sortedBookings);
         setLoading(false);
       } catch (error) {
         setError(error.message);
@@ -24,10 +28,11 @@ const MyBookings = () => {
 
   if (loading)
     return (
-      <p className="text-center pt-5 bg-background1  text-primary_text min-h-screen">
+      <p className="text-center pt-5 bg-background1 text-primary_text min-h-screen">
         Loading...
       </p>
     );
+
   if (error)
     return (
       <p className="text-center bg-background1 pt-5 text-red-500">
@@ -36,15 +41,21 @@ const MyBookings = () => {
     );
 
   return (
-    <div className="container mx-auto p-6 bg-background1 min-h-screen">
+    <div className="container mx-auto p-6 bg-background1 min-h-screen min-w-screen">
       <h1 className="text-4xl font-montserrat font-bold mb-8 text-center text-primary_text">
         My Bookings
       </h1>
-      <ul className="space-y-8">
-        {bookings.map((booking) => (
-          <BookingItem key={booking._id} booking={booking} />
-        ))}
-      </ul>
+      {bookings.length > 0 ? (
+        <ul className="space-y-8">
+          {bookings.map((booking) => (
+            <BookingItem key={booking._id} booking={booking} />
+          ))}
+        </ul>
+      ) : (
+        <p className="text-center text-primary_text">
+          Book a show to get the ticket
+        </p>
+      )}
     </div>
   );
 };
