520d37eab1d7b36651895ce464b9140ac33f4608
Pratik Biswas <04biswaspratik@gmail.com>
2024-07-14 02:40:14 +0530

    skeleton error is resolved

--- client/src/pages/Home/homeCardSlider.jsx
+++ client/src/pages/Home/homeCardSlider.jsx
@@ -1,122 +1,141 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import SkeletonMoviecard from '../../components/Skeletons/SkeletonMovieCard/SkeletonMoviecard';
+import SkeletonMoviecard from "../../components/Skeletons/SkeletonMovieCard/SkeletonMoviecard";
 
 const HomeCardSlider = ({ elements, title, what }) => {
-    const [loading, setLoading] = useState(true);
-    const [imageLoaded, setImageLoaded] = useState(Array(elements.length).fill(false));
-    const handleImageLoad = (index) => {
-        setImageLoaded(prevState => {
-            const newLoadedState = [...prevState];
-            newLoadedState[index] = true;
-            console.log(newLoadedState);
-            return newLoadedState;
-        });
-    };
-    useEffect(() => {
-        setTimeout(() => {
-            setLoading(false);
-        }, 2000);
-    }, []);
-    const settings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        initialSlide: 0,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true,
-                },
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    initialSlide: 2,
-                },
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                },
-            },
-        ],
-    };
+  const [loading, setLoading] = useState(true);
+  const [imageLoaded, setImageLoaded] = useState(
+    Array(elements.length).fill(false)
+  );
+  const handleImageLoad = (index) => {
+    setImageLoaded((prevState) => {
+      const newLoadedState = [...prevState];
+      newLoadedState[index] = true;
+      console.log(newLoadedState);
+      return newLoadedState;
+    });
+  };
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  };
 
-    const visibleElements = elements.slice(0, 7);
-    console.log(elements.length);
+  const visibleElements = elements.slice(0, 7);
+  console.log(elements.length);
 
-    return (
-        <div id="" className="m-auto w-[90%]">
-            <h3 id="" className="text-2xl text-white font-normal">
-                {title}
-            </h3>
-            <div id='movieCards' className="my-5">
-                <Slider {...settings} className="object-center">
-                    {loading
-                        ? Array.from({ length: 7 }).map((_, key) => (
-                            <SkeletonMoviecard key={key} />
-                        ))
-                        : visibleElements.map((element, key) => (
-                            <Link
-                                key={key}
-                                to={
-                                    what === "movies"
-                                        ? `explore/movies/${element.slug}`
-                                        : `explore/shows/${element.slug}`
-                                }
-                            >
-                                <div className="font-normal min-h-[250px] md:min-h-[400px]  ">
-                                    <div className="p-2 flex justify-center items-center flex-wrap ">
-                                        <div
-                                            id="moviePoster"
-                                            className="w-full text-center bg-contain ">
-                                            {elements.length == 0 ? <SkeletonMoviecard /> :
-                                                <div>
-                                                    <img
-                                                        src={element.poster}
-                                                        alt={element.title}
-                                                        onLoad={() => handleImageLoad(key)}
-                                                        className={`
-                                                            ${what === 'movies' ? 'h-[250px] w-[200px] md:h-[400px] md:w-[280px]' : 
-                                                                'h-[250px] w-[200px] md:h-[400px] md:w-[300px]'
+  return (
+    <div id="" className="m-auto w-[90%]">
+      <h3 id="" className="text-2xl text-white font-normal">
+        {title}
+      </h3>
+      <div id="movieCards" className="my-5">
+        <Slider {...settings} className="object-center">
+          {loading
+            ? Array.from({ length: 7 }).map((_, key) => (
+                <SkeletonMoviecard key={key} />
+              ))
+            : visibleElements.map((element, key) => (
+                <Link
+                  key={key}
+                  to={
+                    what === "movies"
+                      ? `explore/movies/${element.slug}`
+                      : `explore/shows/${element.slug}`
+                  }
+                >
+                  <div className="font-normal min-h-[250px] md:min-h-[400px]  ">
+                    <div className="p-2 flex justify-center items-center flex-wrap ">
+                      <div
+                        id="moviePoster"
+                        className="w-full text-center bg-contain "
+                      >
+                        {elements.length == 0 ? (
+                          <SkeletonMoviecard />
+                        ) : (
+                          <div>
+                            <img
+                              src={element.poster}
+                              alt={element.title}
+                              onLoad={() => handleImageLoad(key)}
+                              className={`
+                                                            ${
+                                                              what === "movies"
+                                                                ? "h-[250px] w-[200px] md:h-[400px] md:w-[280px]"
+                                                                : "h-[250px] w-[200px] md:h-[400px] md:w-[300px]"
                                                             } `}
-                                                    />
-                                                    <div className="my-3 font-medium text-xl font-open_sans">
-                                                        <h3>{element.title}</h3>
-                                                    </div>
-                                                </div>
-                                            }
-
-                                        </div>
-                                    </div>
-                                </div>
-                            </Link>
-                        ))}
-                    <div className={` mt-2 rounded-md bg-shadow h-[250px] w-[300px] md:h-[400px] md:w-[400px] !flex !justify-center !items-center !bg-no-repeat relative right-0 ${elements.length != 0 && !loading ? '!block' : '!hidden'} `}>
-                        <Link to={`explore/${what === "movies" ? "movies" : "shows"}`} className={`flex-1 flex justify-center items-center  `}>
-                            <div className={`bg-background2 h-28 w-[50%] rounded-[30px] flex justify-center items-center flex-wrap text-center ${elements.length != 0 && !loading ? 'block' : 'hidden'}`}>
-                                <span className="text-white font-light font-montserrat text-sm md:text-base ">Explore more {what === "movies" ? "movies" : "shows"}</span>
+                            />
+                            <div className="my-3 font-medium text-xl font-open_sans">
+                              <h3>{element.title}</h3>
                             </div>
-                        </Link>
+                          </div>
+                        )}
+                      </div>
                     </div>
-                </Slider>
-            </div>
-        </div>
-    );
+                  </div>
+                </Link>
+              ))}
+          <div
+            className={` mt-2 rounded-md bg-shadow h-[250px] w-[300px] md:h-[400px] md:w-[400px] !flex !justify-center !items-center !bg-no-repeat relative right-0 ${
+              elements.length != 0 && !loading ? "!block" : "!hidden"
+            } `}
+          >
+            <Link
+              to={`explore/${what === "movies" ? "movies" : "shows"}`}
+              className={`flex-1 flex justify-center items-center  `}
+            >
+              <div
+                className={`bg-background2 h-28 w-[50%] rounded-[30px] flex justify-center items-center flex-wrap text-center ${
+                  elements.length != 0 && !loading ? "block" : "hidden"
+                }`}
+              >
+                <span className="text-white font-light font-montserrat text-sm md:text-base ">
+                  Explore more {what === "movies" ? "movies" : "shows"}
+                </span>
+              </div>
+            </Link>
+          </div>
+        </Slider>
+      </div>
+    </div>
+  );
 };
 
 export default HomeCardSlider;
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,191 +1,201 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import Slider from 'react-slick';
-import axiosInstance from '../../config/axiosInstance';
-import HomeCardSlider from './HomeCardSlider';
-import SkeletonLoaderHeader from './Skeletons/SkeletonLoaderHeader';
-import 'react-loading-skeleton/dist/skeleton.css';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import './custom-slick.css';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Slider from "react-slick";
+import axiosInstance from "../../config/axiosInstance";
+import HomeCardSlider from "./homeCardSlider.jsx";
+import SkeletonLoaderHeader from "./Skeletons/SkeletonLoaderHeader";
+import "react-loading-skeleton/dist/skeleton.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./custom-slick.css";
 
 const NextArrow = ({ className, style, onClick }) => {
-    return (
-        <div
-            className={className}
-            style={{ ...style, display: "block", background: "black", width: "60px", height: "60px", borderRadius: "50%", fontSize: "30px" }}
-            onClick={onClick}
-        />
-    );
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        background: "black",
+        width: "60px",
+        height: "60px",
+        borderRadius: "50%",
+        fontSize: "30px",
+      }}
+      onClick={onClick}
+    />
+  );
 };
 
 const PrevArrow = ({ className, style, onClick }) => {
-    return (
-        <div
-            className={className}
-            style={{ ...style, display: "block", background: "black", width: "60px", height: "60px", borderRadius: "50%", fontSize: "30px" }}
-            onClick={onClick}
-        />
-    );
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        background: "black",
+        width: "60px",
+        height: "60px",
+        borderRadius: "50%",
+        fontSize: "30px",
+      }}
+      onClick={onClick}
+    />
+  );
 };
 
 const Home = () => {
-    const [loading, setLoading] = useState(true);
-    const [oldSlide, setOldSlide] = useState(0);
-    const [activeSlide, setActiveSlide] = useState(0);
-    const [activeSlide2, setActiveSlide2] = useState(0);
-    const [recentMovies, setRecentMovies] = useState([]);
-    const [latestMovies, setLatestMovies] = useState([]);
-    const [popularMovies, setPopularMovies] = useState([]);
-    const [shows, setShows] = useState([]);
-    const [moviesLoading, setMoviesLoading] = useState(true);
-    const [showsLoading, setShowsLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
+  const [oldSlide, setOldSlide] = useState(0);
+  const [activeSlide, setActiveSlide] = useState(0);
+  const [activeSlide2, setActiveSlide2] = useState(0);
+  const [recentMovies, setRecentMovies] = useState([]);
+  const [latestMovies, setLatestMovies] = useState([]);
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [shows, setShows] = useState([]);
+  const [moviesLoading, setMoviesLoading] = useState(true);
+  const [showsLoading, setShowsLoading] = useState(true);
 
-    const GetAllMovies = async () => {
-        try {
-            const response = await axiosInstance.get("/movies");
-            const movies = response.data;
-            const recent = movies.slice(Math.max(movies.length - 4, 0));
-            const latest = movies
-                .sort(
-                    (a, b) => new Date(b.releaseDate) - new Date(a.releaseDate)
-                )
-                .slice(0, 8);
-            const popular = movies
-                .sort((a, b) => b.numberOfReviews - a.numberOfReviews)
-                .slice(0, 8);
-            setRecentMovies(recent);
-            setLatestMovies(latest);
-            setPopularMovies(popular);
-        } catch (error) {
-            console.error(error);
-        } finally {
-            setMoviesLoading(false);
-        }
-    };
-
-    const GetAllShows = async () => {
-        try {
-            const response = await axiosInstance.get("/shows");
-            const shows = response.data;
-
-            const currentDate = new Date();
-
-            const upcomingShows = shows
-                .map((show) => {
-                    const [hours, minutes] = show.time.split(":");
-                    const [year, month, day] = show.date
-                        .split("T")[0]
-                        .split("-");
-                    const showDateTime = new Date(
-                        year,
-                        month - 1,
-                        day,
-                        hours,
-                        minutes
-                    );
-                    return { ...show, showDateTime };
-                })
-                .filter((show) => show.showDateTime >= currentDate);
-
-            const sortedUpcomingShows = upcomingShows.sort(
-                (a, b) => a.showDateTime - b.showDateTime
-            );
-
-            const nearestFourShows = sortedUpcomingShows.slice(0, 4);
-
-            setShows(nearestFourShows);
-        } catch (error) {
-            console.error(error);
-        } finally {
-            setShowsLoading(false);
-        }
-    };
-
-    const settings = {
-        dots: true,
-        infinite: true,
-        autoplay: true,
-        speed: 1000,
-        autoplaySpeed: 2000,
-        cssEase: "linear",
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        nextArrow: <NextArrow />,
-        prevArrow: <PrevArrow />,
-        beforeChange: (current, next) => {
-            setOldSlide(current);
-            setActiveSlide(next);
-        },
-        afterChange: (current) => setActiveSlide2(current),
-    };
-
-    useEffect(() => {
-        GetAllMovies();
-        GetAllShows();
-    }, []);
-    useEffect(() => {
-        setLoading(false);
-    }, []);
-    if (loading) {
-        return (
-            <>
-                <SkeletonLoaderHeader />
-            </>
-        );
+  const GetAllMovies = async () => {
+    try {
+      const response = await axiosInstance.get("/movies");
+      const movies = response.data;
+      const recent = movies.slice(Math.max(movies.length - 4, 0));
+      const latest = movies
+        .sort((a, b) => new Date(b.releaseDate) - new Date(a.releaseDate))
+        .slice(0, 8);
+      const popular = movies
+        .sort((a, b) => b.numberOfReviews - a.numberOfReviews)
+        .slice(0, 8);
+      setRecentMovies(recent);
+      setLatestMovies(latest);
+      setPopularMovies(popular);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setMoviesLoading(false);
     }
+  };
 
+  const GetAllShows = async () => {
+    try {
+      const response = await axiosInstance.get("/shows");
+      const shows = response.data;
+
+      const currentDate = new Date();
+
+      const upcomingShows = shows
+        .map((show) => {
+          const [hours, minutes] = show.time.split(":");
+          const [year, month, day] = show.date.split("T")[0].split("-");
+          const showDateTime = new Date(year, month - 1, day, hours, minutes);
+          return { ...show, showDateTime };
+        })
+        .filter((show) => show.showDateTime >= currentDate);
+
+      const sortedUpcomingShows = upcomingShows.sort(
+        (a, b) => a.showDateTime - b.showDateTime
+      );
+
+      const nearestFourShows = sortedUpcomingShows.slice(0, 4);
+
+      setShows(nearestFourShows);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setShowsLoading(false);
+    }
+  };
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    beforeChange: (current, next) => {
+      setOldSlide(current);
+      setActiveSlide(next);
+    },
+    afterChange: (current) => setActiveSlide2(current),
+  };
+
+  useEffect(() => {
+    GetAllMovies();
+    GetAllShows();
+  }, []);
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+  if (loading) {
     return (
-        <div
-            id="Home"
-            className=" w-screen h-fit flex flex-col items-center bg-background1 text-white font-semibold font-montserrat">
-            {loading ?
-                <SkeletonLoaderHeader /> :
-                <header className="h-full w-full my-8 min-h-9">
-                    <div id='headerMovies' className="w-full h-full">
-                        <Slider {...settings} className=" bg-background2 mx-9 object-center">
-                            {recentMovies.map((movie) => (
-                                <Link key={movie.slug} to={`explore/movies/${movie.slug}`}>
-                                    <div className=" min-w-[70%] min-h-[35vw] md:min-h-[30vw]">
-                                        {
-                                            <div className="w-[95%] h-[50vw] md:h-[30vw]" /> &&
-                                            <img
-                                                src={`${movie.coverImage}`}
-                                                alt=""
-                                                className="w-full h-full md:h-[30vw] object-center"
-                                            />
-                                        }
-                                    </div>
-                                </Link>
-                            ))}
-                            
-                        </Slider>
-                    </div>
-                </header>}
-            <div className="w-screen h-full flex justify-center items-center min-h-5">
-                <div className="w-full h-full">
-                    <HomeCardSlider
-                        elements={latestMovies}
-                        title="Latest Movies"
-                        what="movies"
-                        isLoading={moviesLoading}
-                    />
-                    <HomeCardSlider
-                        elements={popularMovies}
-                        title="Popular Movies"
-                        what="movies"
-                        isLoading={moviesLoading}
-                    />
-                    <HomeCardSlider
-                        elements={shows.slice(0, 4)}
-                        title="Upcoming Shows"
-                        what="shows"
-                        isLoading={showsLoading}
-                    />
-                </div>
-            </div>
-        </div>
+      <>
+        <SkeletonLoaderHeader />
+      </>
     );
+  }
+
+  return (
+    <div
+      id="Home"
+      className=" w-screen h-fit flex flex-col items-center bg-background1 text-white font-semibold font-montserrat"
+    >
+      {loading ? (
+        <SkeletonLoaderHeader />
+      ) : (
+        <header className="h-full w-full my-8 min-h-9">
+          <div id="headerMovies" className="w-full h-full">
+            <Slider
+              {...settings}
+              className=" bg-background2 mx-9 object-center"
+            >
+              {recentMovies.map((movie) => (
+                <Link key={movie.slug} to={`explore/movies/${movie.slug}`}>
+                  <div className=" min-w-[70%] min-h-[35vw] md:min-h-[30vw]">
+                    {<div className="w-[95%] h-[50vw] md:h-[30vw]" /> && (
+                      <img
+                        src={`${movie.coverImage}`}
+                        alt=""
+                        className="w-full h-full md:h-[30vw] object-center"
+                      />
+                    )}
+                  </div>
+                </Link>
+              ))}
+            </Slider>
+          </div>
+        </header>
+      )}
+      <div className="w-screen h-full flex justify-center items-center min-h-5">
+        <div className="w-full h-full">
+          <HomeCardSlider
+            elements={latestMovies}
+            title="Latest Movies"
+            what="movies"
+            isLoading={moviesLoading}
+          />
+          <HomeCardSlider
+            elements={popularMovies}
+            title="Popular Movies"
+            what="movies"
+            isLoading={moviesLoading}
+          />
+          <HomeCardSlider
+            elements={shows.slice(0, 4)}
+            title="Upcoming Shows"
+            what="shows"
+            isLoading={showsLoading}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
