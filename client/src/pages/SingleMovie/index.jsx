56fc60f46d60a6fcbd9f20815323a06e72d48aa5
Chandrima Kar <chandrimakar16@gmail.com>
2024-07-16 12:01:23 +0530

    changes updated

--- client/src/pages/SingleMovie/index.jsx
+++ client/src/pages/SingleMovie/index.jsx
@@ -139,12 +139,12 @@ const SingleMovie = () => {
             {/* shadow */}
             <div className="flex flex-row h-full bg-black bg-opacity-75 gap-5">
               {/* left side */}
-              <div className=" h-auto lg:ml-24 md:ml-16 sm:ml-5 ml-1 flex flex-col justify-center items-center xl:w-1/5 lg:w-1/4 md:w-1/5 sm:w-1/4 w-2/5">
+              <div className=" h-auto lg:ml-24 md:ml-16 sm:ml-5 ml-5 flex flex-col justify-center items-center xl:w-1/5 lg:w-1/4 md:w-1/5 sm:w-1/4 w-2/5 sm:mb-0 mb-5">
                 {/* poster image */}
                 <img
                   src={movie.poster}
                   alt={`${movie.title} Poster`}
-                  className="sm:w-auto sm:h-auto w-[65%] h-[90%] mb-2 mt-3 sm:p-2 p-1 rounded-xl "
+                  className="w-auto h-auto mb-2 mt-3 sm:p-2 rounded-xl "
                 />
 
                 <div id="button-div">
@@ -152,14 +152,13 @@ const SingleMovie = () => {
                   <button
                     id="button"
                     onClick={handleClick}
-                    className="group rounded-lg mb-3 flex items-center space-x-0.5 bg-highlight hover:bg-highlight_hover lg:px-[4.5rem] lg:py-2 md:px-6 md:py-2 sm:px-10 sm:py-1 px-2 py-1"
+                    className="group rounded-lg mb-3 flex items-center space-x-0.5 bg-highlight hover:bg-highlight_hover lg:px-[4.5rem] lg:py-2 md:px-6 md:py-2 sm:px-10 sm:py-1 px-0 py-0 hidden sm:flex"
                   >
-                    <span className="sm:font-semibold font-normal tracking-tighter sm:tracking-normal font-ubuntu text-white lg:text-lg md:text-md sm:text-sm text-xs">
+                    <span className="sm:font-semibold font-normal tracking-tighter sm:tracking-normal font-ubuntu text-white lg:text-lg md:text-md sm:text-sm text-sm">
                       Play Now
                     </span>
-
                     <div className="flex items-center translate-x-1 transition-all duration-300">
-                      <BiRightArrow className=" text-white w-3 h-3 lg:w-6 lg:h-5 md:w-4 md:h-4 sm:w-3 sm:h-3" />
+                      <BiRightArrow className="text-white w-3 h-3 lg:w-6 lg:h-5 md:w-4 md:h-4 sm:w-3 sm:h-3" />
                     </div>
                   </button>
                 </div>
@@ -168,48 +167,47 @@ const SingleMovie = () => {
               {/* right side */}
               <div className="flex flex-col justify-center items-start text-white xl:w-2/5 lg:w-2/4 md:w-3/5 sm:w-2/4 w-3/5 lg:gap-5 md:gap-2 sm:gap-1">
                 {/* movie name */}
-                <div className=" tracking-normal text-left lg:text-4xl md:text-xl text-md mb-6 md:mb-1 md:ml-3 sm:ml-1 ml-0 font-montserrat">
+                <div className=" tracking-normal mt-5 sm:mt-0 text-left lg:text-4xl md:text-xl text-base mb-2 md:mb-1 md:ml-3 sm:ml-1 ml-0 font-montserrat">
                   <strong>{movie.title}</strong>
                 </div>
 
-                <div className="flex flex-row items-center">
+                <div className="flex flex-row items-center ">
                   {/* duration */}
-                  <LuDot className="md:w-6 md:h-6 sm:w-3 sm:h-3" />
-                  <div className="lg:text-xl md:text-sm text-xs lg:tracking-normal md:tracking-tight lg:mr-3 md:mr-2 sm:mr-1 font-roboto">
+                  <LuDot className="md:w-6 md:h-6 sm:w-3 sm:h-3 w-2 h-2" />
+                  <div className="lg:text-xl md:text-sm text-base lg:tracking-normal md:tracking-tight lg:mr-3 md:mr-2 sm:mr-1 font-roboto">
                     {movie.duration} hr
                   </div>
 
                   {/* year of release */}
-                  <LuDot className="md:w-6 md:h-6 sm:w-3 sm:h-3" />
-                  <div className="lg:text-xl md:text-sm text-xs lg:tracking-normal md:tracking-tight mr-3 font-roboto">
+                  <LuDot className="md:w-6 md:h-6 sm:w-3 sm:h-3 w-4 h-4" />
+                  <div className="lg:text-xl md:text-sm text-base lg:tracking-normal md:tracking-tight lg:mr-3 mr-0 font-roboto">
                     {moment(movie.releaseDate).year()}
                   </div>
 
                   {/* category */}
-                  <LuDot className="md:w-6 md:h-6 sm:w-3 sm:h-3" />
-                  <div className="lg:text-xl md:text-sm text-xs lg:tracking-normal md:tracking-tight font-roboto">
+                  <LuDot className="md:w-6 md:h-6 sm:w-3 sm:h-3 w-4 h-4" />
+                  <div className="lg:text-xl md:text-sm text-base lg:tracking-normal md:tracking-tight font-roboto">
                     {movie.genres.join(", ")}
                   </div>
                 </div>
 
-                <div className="flex flex-row gap-2 items-center py-3 md:ml-3 sm:ml-0 rounded-[8px]">
+                <div className="flex flex-wrap sm:gap-2 gap-x-3 gap-y-1 items-center py-3 md:ml-3 sm:ml-0 rounded-[8px]">
                   {/* average rating */}
-                  <FaStar className="md:h-4 md:w-4 sm:w-3 sm:h-3 h-2 w-2  text-white" />
-                  <div className="lg:text-xl md:text-sm text-xs lg:tracking-normal md:tracking-tight tracking-tighter font-roboto">
+                  <FaStar className="md:h-4 md:w-4 sm:w-3 sm:h-3 h-4 w-4 text-white" />
+                  <div className="lg:text-xl md:text-sm text-base lg:tracking-normal md:tracking-tight tracking-tighter font-roboto">
                     {movie.averageRating}/5
                   </div>
 
                   {/* total ratings */}
-                  <div className="lg:text-xl md:text-sm text-xs lg:ml-10 sm:ml-5 ml-0 tracking-normal md:tracking-tight font-roboto">
+                  <div className="lg:text-xl md:text-sm text-base lg:ml-10 sm:ml-5 ml-2 tracking-normal md:tracking-tight font-roboto">
                     {movie.reviews.length} votes
                   </div>
 
                   {/* add review button */}
-                  <div className="lg:ml-10 md:ml-5 lg:text-md md:text-sm text-xs sm:ml-3 ml-0">
+                  <div className="lg:ml-10 md:ml-5 lg:text-md md:text-sm text-sm sm:ml-3 ml-0 hidden sm:flex">
                     <button
                       onClick={() => handleReviewRequest()}
-                      className=" bg-highlight hover:bg-highlight_hover text-white sm:font-semibold font-normal font-ubuntu rounded-lg focus:outline-none focus:shadow-outline
-                        sm:py-[.35rem] md:px-6 sm:px-3 px-[4.5px] py-1 sm:tracking-normal tracking-tighter"
+                      className="bg-highlight hover:bg-highlight_hover text-white sm:font-semibold font-normal font-ubuntu rounded-lg focus:outline-none focus:shadow-outline sm:py-[.35rem] md:px-6 sm:px-3 px-3 py-1 tracking-normal"
                     >
                       Add Review
                     </button>
@@ -217,15 +215,41 @@ const SingleMovie = () => {
                 </div>
 
                 {/* movie language */}
-                <div className="bg-white text-black md:px-4 sm:py-1 sm:px-3 px-2 md:ml-3 sm:ml-0 lg:text-lg md:text-sm text-xs mb-2 tracking-wide font-roboto">
+                <div className="bg-white text-black md:px-4 sm:py-1 sm:px-3 px-4 md:ml-3 sm:ml-0 lg:text-lg md:text-sm text-base sm:mb-2 mb-7 tracking-wide font-roboto">
                   <strong>{movie.language}</strong>
                 </div>
               </div>
             </div>
           </div>
 
+          <div className="px-8 mt-3 flex flex-row justify-between self-stretch sm:hidden">
+            <div id="button-div">
+              {/* play now button */}
+              <button
+                id="button"
+                onClick={handleClick}
+                className="group rounded-lg mb-3 flex items-center space-x-0.5 bg-highlight hover:bg-highlight_hover px-6 py-2"
+              >
+                <span className="font-semibold tracking-normal font-ubuntu text-white text-sm">
+                  Play Now
+                </span>
+                <div className="flex items-center translate-x-1 transition-all duration-300">
+                  <BiRightArrow className="text-white w-3 h-3 lg:w-6 lg:h-5 md:w-4 md:h-4 sm:w-3 sm:h-3" />
+                </div>
+              </button>
+            </div>
+            <div className=" text-sm">
+              <button
+                onClick={() => handleReviewRequest()}
+                className="bg-highlight hover:bg-highlight_hover text-white font-semibold font-ubuntu rounded-lg focus:outline-none focus:shadow-outline px-6 py-2 tracking-normal"
+              >
+                Add Review
+              </button>
+            </div>
+          </div>
+
           {/* Movie Description */}
-          <div className="lg:px-10 md:px-2 sm:px-5 px-1 sm:mt-10 mt-3 flex flex-col gap-y-10">
+          <div className="lg:px-10 md:px-2 sm:px-5 px-1 sm:mt-10 mt-0 flex flex-col gap-y-8">
             <div className=" text-left text-white flex flex-col gap-5 common-container md:px-16 sm:px-5 px-10">
               <div className="sm:text-2xl text-base font-bold font-montserrat tracking-wider common-heading">
                 Description:
