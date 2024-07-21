1264513c4c6f4ac058b7a61a21e5826f66ad878f
Chandrima Kar <chandrimakar16@gmail.com>
2024-07-22 00:13:36 +0530

    Search button is used

--- client/src/pages/AdminUsers/index.jsx
+++ client/src/pages/AdminUsers/index.jsx
@@ -4,12 +4,16 @@ import UserCard from "../../components/userCard";
 import axiosInstance from "../../config/axiosInstance";
 import Preloader from "../../components/PreLoader/PreLoader";
 import { toast } from "sonner";
+import { FaSearch } from "react-icons/fa";
 
 const AdminUsers = () => {
   const [adminUsers, setAdminUsers] = useState([]);
   const [nonAdminUsers, setNonAdminUsers] = useState([]);
+  const [filteredAdminUsers, setFilteredAdminUsers] = useState([]);
+  const [filteredNonAdminUsers, setFilteredNonAdminUsers] = useState([]);
   const [loading, setLoading] = useState(true); // Initialize loading state
   const [isInitialLoad, setIsInitialLoad] = useState(true); // Track initial load
+  const [searchQuery, setSearchQuery] = useState("");
 
   const currentUser = useSelector((state) => state.user.userInfo);
   const isOwner = currentUser.isOwner;
@@ -26,6 +30,8 @@ const AdminUsers = () => {
 
       setAdminUsers(admins);
       setNonAdminUsers(nonAdmins);
+      setFilteredAdminUsers(admins);
+      setFilteredNonAdminUsers(nonAdmins);
       setLoading(true);
     } catch (error) {
       console.error("Error fetching users:", error);
@@ -47,6 +53,27 @@ const AdminUsers = () => {
 
     fetchData();
   }, []); // Run once on component mount
+
+  useEffect(() => {
+    searchUsers();
+  }, [searchQuery, adminUsers, nonAdminUsers]);
+
+  const searchUsers = () => {
+    let tempAdminUsers = [...adminUsers];
+    let tempNonAdminUsers = [...nonAdminUsers];
+
+    if (searchQuery) {
+      tempAdminUsers = tempAdminUsers.filter((user) =>
+        user.name.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+      tempNonAdminUsers = tempNonAdminUsers.filter((user) =>
+        user.name.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+    }
+
+    setFilteredAdminUsers(tempAdminUsers);
+    setFilteredNonAdminUsers(tempNonAdminUsers);
+  };
 
   const handleAdminAuthorize = async (userId, userAdminStatus) => {
     if (!isOwner) return;
@@ -88,14 +115,29 @@ const AdminUsers = () => {
 
   return (
     <div className="container mx-auto p-4 min-h-screen">
+      <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-y-3 mb-4">
+        <h1 className="text-4xl font-semibold text-primary_text py-4 tracking-tighter font-playfair">
+          Admin User Management
+        </h1>
+        <div className="relative flex items-center">
+          <input
+            type="text"
+            placeholder="Search for users..."
+            className="text-primary_text bg-shadow rounded-lg focus:outline-none focus:border focus:border-highlight py-1 text-xs sm:text-base pl-10 sm:pl-10 mx-1 sm:px-4"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <FaSearch className="absolute left-3 text-primary_text w-4 h-4" />
+        </div>
+      </div>
       {/* Admin Users */}
       {isOwner && (
         <>
-          <h1 className="text-4xl font-semibold text-primary_text mb-4 tracking-tighter  font-playfair">
+          <h1 className="text-4xl font-semibold text-primary_text mb-4 tracking-tighter font-playfair">
             All Admins
           </h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4">
-            {adminUsers.map((user) => (
+            {filteredAdminUsers.map((user) => (
               <UserCard
                 key={user._id}
                 user={user}
@@ -112,7 +154,7 @@ const AdminUsers = () => {
         All Users
       </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4">
-        {nonAdminUsers.map((user) => (
+        {filteredNonAdminUsers.map((user) => (
           <UserCard
             key={user._id}
             user={user}
